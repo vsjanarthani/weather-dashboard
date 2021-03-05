2947b3209d2b688203359b113a7af3286fa9d372
jana <vs.janarthani@gmail.com>
2021-03-05 15:30:25 -0800

    added basis HTML & CSS codes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,15 @@
-// Weather Dashboard
+// Initialising DOM Elements
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+
+
+// Function to search and fetch data
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  const ApiKey = "c42b97fce8e9798a49e614a426da209d";
+
+  fetch(`api.openweathermap.org/data/2.5/weather?q={inputValue}&appid={ApiKey}`)
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error('err'))
+});
